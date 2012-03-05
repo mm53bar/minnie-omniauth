ce951d3e883d26b529e241a7bca6fd9301bf3425
Michael McClenaghan <mike@sideline.ca>
2012-03-05 16:54:13 -0700

    Don't need Sessions#new so removing from before_filter

--- lib/generators/minnie_omniauth/templates/sessions_controller.rb
+++ lib/generators/minnie_omniauth/templates/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_filter :authenticate_user!, :only => [:new, :create]
+  skip_before_filter :authenticate_user!, :only => [:create]
 
   def create
     @user = User.authenticate(auth_hash)
